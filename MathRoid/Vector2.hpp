0d76c8e32c02255bd0418eed9d45b90f6654cf52
tntube <stanis.kapus@gmail.com>
2024-09-17 11:53:56 +0200

    Fix vector2 impl

--- MathRoid/Vector2.hpp
+++ MathRoid/Vector2.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
+#include <cmath>
+
 namespace Math
 {
 	template<typename T>
 	class Vector2
 	{
 	public:
-		Vector2(T x, T y);
+		Vector2(T x, T y) : x(x), y(y) {}
 		~Vector2() = default;
 
 		T x, y;
 
-		float Distance(Vector2 a, Vector2 b);
+		inline float Distance(Vector2 a, Vector2 b) const
+		{
+			return std::sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+		}
 		
 	};
 
